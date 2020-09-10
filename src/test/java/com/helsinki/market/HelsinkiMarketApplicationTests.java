40d7ce4f19b9a3924bc276c91a35a7e330f9071d
Alejandro <serafinalejandroserafin@gmail.com>
2020-09-09 21:11:59 -0500

    Base components (Spring)

---
+++ src/test/java/com/helsinki/market/HelsinkiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helsinki.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelsinkiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
